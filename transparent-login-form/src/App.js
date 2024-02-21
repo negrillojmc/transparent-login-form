1363c863db675b921f6fe115419a793d10fd2ddc
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-02-22 02:00:39 +0800

    Added React and SASS files to repository

---
+++ transparent-login-form/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginForm from './Components/LoginForm/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
